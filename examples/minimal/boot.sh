b2b71838a0d3a2df981ace368834d5015ad38e35
Gregg Reynolds <reynolds-gregg@norc.org>
2016-06-26 20:17:51 -0500

    iniial commit

--- examples/minimal/boot.sh
+++ examples/minimal/boot.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
-set -x
-JLP=${IOTIVITY_HOME}/out/darwin/x86_64/release
+#set -x
 
+IOT=/usr/local/lib/iotivity/1.1
+
+CHIBI=${IOTIVITY_HOME}/out/darwin/x86_64/release
+
+JLP="$IOT"
+
+if [ -d "$IOTIVITY_HOME" ];
+then
+        if [ -d "$JLP" ]
+	then
+	        echo "Starting repl with java.library.path = $JLP"
+	else
+	        echo "java.library.path not found: $JLP";
+	        exit
+	fi
+else
+	echo "IOTIVITY_HOME ($IOTIVITY_HOME) not found."
+	exit
+fi
 
 if [ -f .boot-jvm-options ]; then
   OPTS=`cat .boot-jvm-options`
 fi
 
-BOOT_JVM_OPTIONS="$OPTS -Djava.library.path=$JLP" boot "$@"
+export DYLD_LIBRARY_PATH=$JLP
+
+BOOT_JVM_OPTIONS="$OPTS -Djava.library.path='$JLP'" boot "$@"
--- examples/minimal/build.boot
+++ examples/minimal/build.boot
@@ -4,8 +4,8 @@
 (set-env! :resource-paths #{"resources" "src"}
           :source-paths   #{"test"}
           :dependencies   '[[org.clojure/clojure "RELEASE"]
-                            ;; [net.n01se/clojure-jna "1.0.0"]
-                            [iotivity/jvm "0.1.0-SNAPSHOT"]
+                            [org.clojure/tools.namespace "0.3.0-alpha3"]
+                            [iotivity/iotivity-api "1.1.10-SNAPSHOT"]
                             [org.apache.commons/commons-daemon "1.0.9"]
                             [adzerk/boot-test "RELEASE" :scope "test"]])
 
